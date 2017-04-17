809d384d35a7db5036b258803c7ad868282b218a
allisonmayo <allison.mayo@jointheleague.org>
2017-04-17 14:29:46 -0700

    Add files via upload

--- src/robot/RobotSquare.java
+++ src/robot/RobotSquare.java
@@ -6,22 +6,25 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSquare {
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
-
+    	Robot leia=new Robot();
+    
 
         // 3. Put the robot's pen down
-
+leia.penDown();
 
         // 6. Make the robot move as fast as possible
-
+leia.setSpeed(90);
 
         // 5. Do everything below here 4 times
+for (int i = 0; i < 4; i++) {
+	
 
 
         //         2. Move your robot 200 pixels
-
+leia.move(400);
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
+leia.turn(90);
+}
     }
 }
---
+++ src/robot/name.java
@@ -0,0 +1,19 @@
+package robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class name {
+public static void main(String[] args) {
+	Robot leia=new Robot();
+				leia.penDown();
+				leia.turn(35);
+				leia.move(150);
+				leia.turn(125);
+				leia.move(150);
+				leia.turn(90);
+				
+}
+
+
+}
+
